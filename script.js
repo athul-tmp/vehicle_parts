7b484ae351c76958c35ae392b61055d9c5ce091a
Athul <s3958556@student.rmit.edu.au>
2024-06-22 22:43:42 +1000

    Implemented Model options loading Model options load after make is selected

--- script.js
+++ script.js
@@ -9,6 +9,27 @@ function loadMakes() {
         });
 }
 
+function loadModels() {
+    let make = document.getElementById("make").value;
+    if (make === "") {
+        document.getElementById("model-container").innerHTML = "";
+        return;
+    }
+    fetch(`database.php?action=get_models&make=${encodeURIComponent(make)}`)
+        .then(response => response.json())
+        .then(data => {
+            let modelContainer = document.getElementById("model-container");
+            modelContainer.innerHTML = `Model:
+                                    <select id="model" onchange="loadTypes()">
+                                        <option value="">Select Model</option>
+                                    </select>`;
+            let modelSelect = document.getElementById("model");
+            data.forEach(model => {
+                modelSelect.innerHTML += `<option value="${model}">${model}</option>`;
+            });
+        });
+}
+
 window.onload = function () {
     loadMakes();
 };
