2a30fc8261d09bda3b685111ce485d47012dd764
Athul <s3958556@student.rmit.edu.au>
2024-06-22 22:28:09 +1000

    Implemented complete PHP backend PHP script handles fetching makes, models, types, and part numbers from the database.

--- database.php
+++ database.php
@@ -25,6 +25,48 @@ switch ($action){
         echo json_encode($makes);
         break;
 
+    case 'get_models':
+        $make = $_GET["make"];
+        $query = "SELECT DISTINCT Model FROM vehicle_parts WHERE Make='$make';";
+        $result = $conn->query($query);
+
+        $models = array();
+        while($row = $result->fetch_assoc()) {
+            $models[] = $row['Model'];
+        }
+
+        echo json_encode($models);
+        break;
+
+    case 'get_types':
+        $make = $_GET["make"];
+        $model = $_GET["model"];
+        $query = "SELECT DISTINCT Type FROM vehicle_parts WHERE Make='$make' AND Model='$model';";
+        $result = $conn->query($query);
+
+        $types = array();
+        while($row = $result->fetch_assoc()) {
+            $types[] = $row['Type'];
+        }
+
+        echo json_encode($types);
+        break;
+
+    case 'get_partNo':
+        $make = $_GET["make"];
+        $model = $_GET["model"];
+        $type = $_GET["type"];
+        $query = "SELECT DISTINCT PartNo FROM vehicle_parts WHERE Make='$make' AND Model='$model' AND Type='$type';";
+        $result = $conn->query($query);
+
+        $partNo = array();
+        while($row = $result->fetch_assoc()) {
+            $partNo[] = $partNo['PartNo'];
+        }
+
+        echo json_encode($partNo);
+        break;
+
     default:
         echo json_encode(array("Error" => "Invalid action"));
 }
